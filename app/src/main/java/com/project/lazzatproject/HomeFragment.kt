584af101c60b209a97d1a6b2defa59c6c8f77c23
Faisal Mushtaq <faisalmushtaq781@gmail.com>
2019-02-14 19:24:11 +0530

    new things

--- app/src/main/java/com/project/lazzatproject/HomeFragment.kt
+++ app/src/main/java/com/project/lazzatproject/HomeFragment.kt
@@ -11,6 +11,9 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
 
 
 
@@ -22,47 +25,49 @@ class HomeFragment : Fragment(),View.OnClickListener {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        var view = inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+//
+        val myFab = view.findViewById(R.id.addCategory) as FloatingActionButton
+        myFab.setOnClickListener {
+            val alert = AlertDialog.Builder(context)
+            var category_name: EditText?=null
+// Builder
+            with (alert) {
+                setTitle("Add category")
+                setMessage("Name of the category")
+// Add any  input field here
+                category_name=EditText(context)
+                category_name!!.hint="Category name"
+                category_name!!.inputType = InputType.TYPE_CLASS_TEXT
+                setPositiveButton("OK") { dialog, whichButton ->
+                    var Category = category_name!!.text.toString()
+                    if (Category != ""){
+                        Toast.makeText(context, Category + " category created", Toast.LENGTH_SHORT).show()
+                    }
+                    //showMessage("display the game score or anything!")
+                    dialog.dismiss()
 
-        var button = view.findViewById(R.id.fab_add) as ImageButton
-        button.setOnClickListener(this)
+                }
+                setNegativeButton("NO") {
+                    dialog, whichButton ->
+
+                    //showMessage("Close the game or anything!")
+                    dialog.dismiss()
+                }
+            }
+// Dialog
+            val dialog = alert.create()
+            dialog.setView(category_name)
+            dialog.show()
+        }
         return view
 
 
     }
     override fun onClick(v: View) {
 
-        val alert = AlertDialog.Builder(context)
-        var category_name: EditText?=null
-// Builder
-        with (alert) {
-            setTitle("Add category")
-            setMessage("Name of the category")
-// Add any  input field here
-            category_name=EditText(context)
-            category_name!!.hint="Category name"
-            category_name!!.inputType = InputType.TYPE_CLASS_TEXT
-            setPositiveButton("OK") { dialog, whichButton ->
-                var Category = category_name!!.text.toString()
-                if (Category != ""){
-                    Toast.makeText(context, Category + " category created", Toast.LENGTH_SHORT).show()
-                }
-                //showMessage("display the game score or anything!")
-                dialog.dismiss()
 
-            }
-            setNegativeButton("NO") {
-                dialog, whichButton ->
-
-                //showMessage("Close the game or anything!")
-                dialog.dismiss()
-            }
-        }
-// Dialog
-        val dialog = alert.create()
-        dialog.setView(category_name)
-        dialog.show()
 
     }
 
