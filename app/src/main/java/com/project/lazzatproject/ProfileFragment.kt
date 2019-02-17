8be5f997dc1e88820d87d71a2867015036590be1
hannanparvez <darhannan5@gmail.com>
2019-02-17 22:40:13 +0530

    menu commit

--- app/src/main/java/com/project/lazzatproject/ProfileFragment.kt
+++ app/src/main/java/com/project/lazzatproject/ProfileFragment.kt
@@ -5,14 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_user_profile.*
 
 
 class ProfileFragment : Fragment() {
+    private var database = FirebaseDatabase.getInstance()
+    private var myRef = database.reference
+    private var mAuth: FirebaseAuth? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //just change the fragment_dashboard
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        mAuth = FirebaseAuth.getInstance()
+        update()
         return inflater.inflate(R.layout.fragment_profile, null)
+
+    }
+    fun update(){
+        var currentuser=mAuth!!.currentUser
+
+
+        if (currentuser!=null){
+            val mref=myRef.child("Users").child(currentuser!!.uid)
+            mref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    val username = dataSnapshot.child("name").value as String
+
+                    user_profile_name.text = username
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                }
+            })
+        }
     }
 }
--- app/src/main/java/com/project/lazzatproject/UserProfileFragment.kt
+++ app/src/main/java/com/project/lazzatproject/UserProfileFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
