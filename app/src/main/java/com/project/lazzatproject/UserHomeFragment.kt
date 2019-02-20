8ee8bc12785da3284e487f4a067baf5100f3c496
hannan <darhannan5@gmail.com>
2019-02-20 23:55:32 +0530

    initial

--- app/src/main/java/com/project/lazzatproject/UserHomeFragment.kt
+++ app/src/main/java/com/project/lazzatproject/UserHomeFragment.kt
@@ -1,17 +1,27 @@
 package com.project.lazzatproject
 
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.util.*
 
 
 class UserHomeFragment : Fragment() {
+
+
+    private var database = FirebaseDatabase.getInstance()
+    private var myRef = database.reference
 
     private lateinit var v: View
     private lateinit var recyclerView: RecyclerView
@@ -20,50 +30,61 @@ class UserHomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-         v = inflater.inflate(R.layout.fragment_user_home, container, false)
+        v = inflater.inflate(R.layout.fragment_user_home, container, false)
         recyclerView = v.findViewById<View>(R.id.recycler_view) as RecyclerView
         val viewAdapter = RecycleViewAdapter(this.context!!, listCont)
         recyclerView.layoutManager = LinearLayoutManager(this.activity)
         recyclerView.adapter = viewAdapter
+
         return v
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Log.d(ContentValues.TAG, "inside onCreate(): ")
 
         listCont = ArrayList()
-        listCont.add(Restaurant("My Place", "Food!!!", R.drawable.fifth))
+        Log.d(ContentValues.TAG, "above myPlace: ")
 
-        listCont.add(Restaurant("Lazzat Cafe", "Anything Available Here!!",R.drawable.first ))
-
-        listCont.add(Restaurant("Hotel Pine", "Anything Available Here!!",R.drawable.second ))
-
-        listCont.add(Restaurant("Smoke 'n Joes", "All the varieties of pizzaa!!",R.drawable.twelveth ))
-
-
-        listCont.add(Restaurant("hotel Khyber", "Whole Kashmiri Cusiene!!",R.drawable.thirteenth ))
-
-
-        listCont.add(Restaurant("Erina", "Ice creams and milk shakes!!",R.drawable.fourtheenth ))
-
-
-        listCont.add(Restaurant("Hotel Madina", "Tea and Cake!!",R.drawable.fifteenth))
+        loadPost()
 
 
 
-        listCont.add(Restaurant("Smoke 'n Joes", "All the varieties of pizzaa!!",R.drawable.twelveth ))
 
+    }
+    private fun loadPost(){
 
-        listCont.add(Restaurant("hotel Khyber", "Whole Kashmiri Cusiene!!",R.drawable.thirteenth ))
+        myRef.child("Users").addValueEventListener(object : ValueEventListener {
 
+            override fun onDataChange(p0: DataSnapshot) {
+                try {
+                    Log.d(ContentValues.TAG, "inside onDataChange(): ")
 
-        listCont.add(Restaurant("Erina", "Ice creams and milk shakes!!",R.drawable.fourtheenth ))
+                    val td = p0.value as HashMap<*, *>
 
+                    for(key in td.keys){
+                        Log.d(ContentValues.TAG, "inside for loop: ")
 
-        listCont.add(Restaurant("Hotel Madina", "Tea and Cake!!",R.drawable.fifteenth))
+                        val post = td[key] as HashMap<*, *>
+                        if(post["type"] as String == "owner") {
+                            Log.d(ContentValues.TAG, "inside if(): ")
+                            listCont.add(Restaurant(post["name"] as String, "Food!!!", R.drawable.fifth))
+                        }
+                    }
 
+                }catch (ex:Exception){}
+                if (isVisible){
 
+                    val viewAdapter = RecycleViewAdapter(context!!,listCont)
+                    recyclerView.adapter = viewAdapter
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 
 }
